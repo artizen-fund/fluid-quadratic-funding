f882facd26f4bceeccbaea24a86baf3b020b9023
Eric Katerman <eric@artizen.fund>
2022-06-07 06:11:06 -0700

    initial commit with partial cookiecutter scaffolding

---
+++ fluid-quadratic-funding/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is an open-source implementation of fluid quadratic funding, a continuous form of public goods funding that leverages QF to distribute funds in an equitable way',
+    author='artizen-fund',
+    license='MIT',
+)
